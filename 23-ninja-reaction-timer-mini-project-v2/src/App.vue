2df2567a22f6f1287b0eb2b1dc58637545de9aea
danielOuattara <daniel.ouattara@gmx.com>
2023-01-09 20:04:43 +0100

    folder rename + logic  updated + formating

--- 23-ninja-reaction-timer-mini-project-v2/src/App.vue
+++ 23-ninja-reaction-timer-mini-project-v2/src/App.vue
@@ -1,21 +1,22 @@
 <template>
-  <h1>{{title}}</h1> <br>
-  <button :disabled="isPlaying" @click="play">Play</button>
-  <button :diabled="deltaTime > 0" @click="reset">Reset</button> <br>
-  <Pusher v-if="isPlaying" :delay="delay" @receiveDeltaTime="storeDeltaTime"  >
+  <h1>{{ title }}</h1>
+  <p>
+    <button :disabled="isPlaying" @click="play">Play</button>
+    <button :diabled="deltaTime > 0" @click="reset">Reset</button>
+  </p>
+  <Pusher v-if="isPlaying" :delay="delay" @receiveDeltaTime="storeDeltaTime">
   </Pusher>
   <Result v-if="showResult" :deltaTime="deltaTime"></Result>
 
   <!-- MY_TODO : finish result of the game -->
-
 </template>
 
 <script>
-import Pusher from './components/Pusher.vue'
-import Result from './components/Result.vue'
+import Pusher from "./components/Pusher.vue";
+import Result from "./components/Result.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Result,
     Pusher,
@@ -24,14 +25,14 @@ export default {
     return {
       title: "Ninja Reaction Timer !",
       isPlaying: false,
-      delay: null ,
+      delay: null,
       deltaTime: null,
       showResult: false,
-    }
+    };
   },
-  methods: {   
+  methods: {
     play() {
-      this.delay = 500 + Math.round(Math.random() * 5000);
+      this.delay = 500 + Math.round(Math.random() * 1000);
       this.isPlaying = true;
       this.deltaTime = null;
     },
@@ -39,16 +40,17 @@ export default {
       this.deltaTime = arg;
       this.showResult = true;
       this.isPlaying = false;
-    }, 
+    },
     reset() {
-      this.deltaTime = 0
-    }
-  }
-}
+      this.deltaTime = 0;
+    },
+  },
+};
 </script>
 
-<style >
-#app, .modals {
+<style>
+#app,
+.modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -68,11 +70,26 @@ export default {
   cursor: pointer;
 }
 h1 {
-  border-bottom: 1px solid  #dddd;
+  border-bottom: 1px solid #dddd;
   display: inline-block;
   padding-bottom: 10px;
   color: blue;
 }
-
-
+button {
+  color: rgb(255, 255, 255);
+  background-color: #535353;
+  padding: 8px 30px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+  text-decoration: none;
+  display: inline-block;
+  /* border: none; */
+  border-style: none;
+}
+button[disabled] {
+  opacity: 0.4;
+  cursor: not-allowed;
+}
 </style>
---
+++ 23-ninja-reaction-timer-mini-project-v2/src/components/Pusher.vue
@@ -0,0 +1,36 @@
+<template>
+  <div type="button" class="pusher" v-if="showPusher" @click="stopTimer">
+    <h1>Ninja !</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["delay" /*  'play', 'showPusher', 'stopTimer' */],
+  data() {
+    return {
+      showPusher: false,
+      stopTime: null,
+      deltaTime: null,
+    };
+  },
+
+  mounted() {
+    setTimeout(this.startTimer, this.delay);
+  },
+
+  methods: {
+    startTimer() {
+      this.showPusher = true;
+      this.startTime = new Date();
+    },
+
+    stopTimer() {
+      this.deltaTime = new Date() - this.startTime;
+      this.$emit("receiveDeltaTime", this.deltaTime);
+    },
+  },
+};
+</script>
+
+<style></style>
