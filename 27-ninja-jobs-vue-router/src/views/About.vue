ea673cc6480646d637733197201743bbd7d8bc17
danielOuattara <daniel.ouattara@gmx.com>
2023-01-10 22:12:06 +0100

    vue router: ninja jobs

---
+++ 27-ninja-jobs-vue-router/src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>About page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, laudantium incidunt? Ab temporibus vero ducimus! Impedit non numquam, repellendus voluptas soluta alias obcaecati quo, tenetur fugit dolorum veritatis nemo eveniet.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, laudantium incidunt? Ab temporibus vero ducimus! Impedit non numquam, repellendus voluptas soluta alias obcaecati quo, tenetur fugit dolorum veritatis nemo eveniet.</p>
+  </div>
+</template>
---
+++ 27-ninja-jobs-vue-router/src/views/jobs/JobDetails.vue
@@ -0,0 +1,23 @@
+<template>
+<h1>Job Details Page</h1>
+<p>Job id is {{ $route.params.id}}</p>
+<p>Job id is {{ this.id}}</p>
+<!-- <p>{{job.details}}</p> -->
+  
+</template>
+
+<script>
+export default {
+    props: ["id"],
+    // data() {
+    //     return {
+    //         id: this.$route.params.id,
+    //     }
+    // }  // No need no more to get the id. Props are best.
+
+}
+</script>
+
+<style>
+
+</style>
