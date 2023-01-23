a9e399597212a1121af6273601721dd0a7d759f7
danielOuattara <daniel.ouattara@gmx.com>
2023-01-23 18:53:11 +0100

    blog project boilerplate

---
+++ 30-composition-blog-project-startup/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">{{ name }} - {{ age }}</div>
+  <button @click="handleClick">click</button>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  components: {},
+  setup() {
+    let name = "Daniel";
+    let age = 39;
+
+    function handleClick() {
+      console.log("You clicked !");
+    }
+
+    return { name, age, handleClick };
+  },
+};
+</script>
