4c497637f53d67163fed819950441663f202c8b1
danielOuattara <daniel.ouattara@gmx.com>
2023-01-23 18:54:17 +0100

    composition refs fucntion for reactivity

---
+++ 32-composition-using-refs-for-reactivity/src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p ref="p_ref">My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="changeName">Click to change Name</button>
+  </div>
+
+  <p>
+    <input type="text" placeholder="name" v-model="name" />
+  </p>
+  <p>
+    <button @click="age--">decrease age</button>
+    <button @click="age++">increase age</button>
+  </p>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "HomeView",
+  components: {},
+  setup() {
+    let name = ref("Daniel");
+    console.log("name = ", name);
+    let age = ref(39);
+
+    function changeName() {
+      name.value = "Julie";
+      age.value = 38;
+      console.log("name = ", name.value, "age = ", age.value);
+      return 0;
+    }
+
+    return { name, age, changeName };
+  },
+};
+</script>
