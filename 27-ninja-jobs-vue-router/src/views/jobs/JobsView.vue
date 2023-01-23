0b18e1e8bca0536504399a3ada6460fcdaa8e450
danielOuattara <daniel.ouattara@gmx.com>
2023-01-23 18:52:15 +0100

    logic update

--- 27-ninja-jobs-vue-router/src/views/jobs/JobsView.vue
+++ 27-ninja-jobs-vue-router/src/views/jobs/JobsView.vue
@@ -1,51 +1,51 @@
 <template>
- <h1>Jobs</h1>
- <div class="job" v-for="job in jobs" :key="job.id">
-  <ul>
-   <li>
-    <router-link :to="{name: 'JobDetails', params: {id: job.id}}">
-     <h2>{{job.title }}</h2>
-    </router-link>
-    <!-- <router-link to="/jobs/" + job.id> {{job.title }} {{job.id}} </router-link> -->
-   </li>
-  </ul>
- </div>
+  <h1>Jobs</h1>
+  <div class="job" v-for="job in jobs" :key="job.id">
+    <ul>
+      <li>
+        <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+          <h2>{{ job.title }}</h2>
+        </router-link>
+        <!-- <router-link to="/jobs/" + job.id> {{job.title }} {{job.id}} </router-link> -->
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
- data() {
-  return {
-   jobs: [
-    { title: "UX Designer", id: 1, details: "lorem" },
-    { title: "Frontend Developper", id: 2, details: "lorem" },
-    { title: "Backend Developper", id: 3, details: "lorem" },
-   ],
-  };
- },
+  data() {
+    return {
+      jobs: [
+        { title: "UX Designer", id: 1, details: "lorem" },
+        { title: "Frontend Developper", id: 2, details: "lorem" },
+        { title: "Backend Developper", id: 3, details: "lorem" },
+      ],
+    };
+  },
 };
 </script>
 
 <style>
 .job h2 {
- background-color: #f4f4f4;
- padding: 20px;
- border-radius: 10px;
- margin: 10px auto;
- max-width: 600px;
- cursor: pointer;
- color: #444;
+  background-color: #c9c9c9;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
 }
 
 .job h2:hover {
- background: #ddd;
+  background: #ddd;
 }
 
 .job a {
- text-decoration: none;
+  text-decoration: none;
 }
 
 ul {
- list-style-type: none;
+  list-style-type: none;
 }
 </style>
