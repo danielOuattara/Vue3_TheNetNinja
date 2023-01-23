212af2718c0c3b584ecea9e76db6d932c7c3f6c2
danielOuattara <daniel.ouattara@gmx.com>
2023-01-23 18:52:29 +0100

    fetching data  with vue3

--- 28-fetching-data/src/views/jobs/JobDetailsView.vue
+++ 28-fetching-data/src/views/jobs/JobDetailsView.vue
@@ -1,28 +1,25 @@
 <template>
- <h1 v-if="job">{{ job.title }}</h1>
- <p>Job id: {{ this.id}}</p>
- <p v-if="job">Details: {{ job.details }}</p>
+  <h1 v-if="job">{{ job.title }}</h1>
+  <p>Job id: {{ this.id }}</p>
+  <p v-if="job">Details: {{ job.details }}</p>
 </template>
 
 <script>
 export default {
- props: ["id"],
- data() {
-  return {
-   // No need no more to get the id. Props are best.
-   // id: this.$route.params.id,
-   job: null,
-  };
- },
+  props: ["id"],
+  data() {
+    return {
+      job: {},
+    };
+  },
 
- created() {
-  fetch(`http://localhost:3000/jobs/${this.id}`)
-   .then((response) => response.json())
-   .then((data) => (this.job = data))
-   .catch((error) => console.log(error));
- },
+  created() {
+    fetch(`http://localhost:8000/jobs/${this.id}`)
+      .then((response) => response.json())
+      .then((data) => (this.job = data))
+      .catch((error) => console.log(error));
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- 28-fetching-data/src/views/jobs/JobsView.vue
+++ 28-fetching-data/src/views/jobs/JobsView.vue
@@ -1,57 +1,54 @@
 <template>
- <h1>Jobs</h1>
- <div class="job" v-for="job in jobs" :key="job.id">
-  <ul>
-   <li>
-    <router-link :to="{name: 'JobDetails', params: {id: job.id}}">
-     <h2>{{job.title }}</h2>
-    </router-link>
-    <!-- <router-link to="/jobs/" + job.id> {{job.title }} {{job.id}} </router-link> -->
-   </li>
-  </ul>
- </div>
+  <h1>Jobs</h1>
+  <div class="job" v-for="job in jobs" :key="job.id">
+    <ul>
+      <li>
+        <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+          <h2>{{ job.title }}</h2>
+        </router-link>
+        <!-- <router-link to="/jobs/" + job.id> {{job.title }} {{job.id}} </router-link> -->
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
- data() {
-  return {
-   jobs: [],
-  };
- },
+  data() {
+    return {
+      jobs: [],
+    };
+  },
 
- created() {
-  fetch("http://localhost:3000/jobs")
-   .then((response) => response.json())
-   .then((data) => {
-    console.log(data);
-    this.jobs = data;
-   })
-   .catch((error) => console.log(error));
- },
+  created() {
+    fetch("http://localhost:8000/jobs")
+      .then((response) => response.json())
+      .then((data) => (this.jobs = data))
+      .catch((error) => console.log(error));
+  },
 };
 </script>
 
 <style>
 .job h2 {
- background-color: #f4f4f4;
- padding: 20px;
- border-radius: 10px;
- margin: 10px auto;
- max-width: 600px;
- cursor: pointer;
- color: #444;
+  background-color: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
 }
 
 .job h2:hover {
- background: #ddd;
+  background: #ddd;
 }
 
 .job a {
- text-decoration: none;
+  text-decoration: none;
 }
 
 ul {
- list-style-type: none;
+  list-style-type: none;
 }
 </style>
